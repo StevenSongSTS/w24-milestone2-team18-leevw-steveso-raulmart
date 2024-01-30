beee4fd21799231d2064a88adca6e575ac5b85cf
Raul Martinez <raulmartinez1855@gmail.com>
2024-01-29 19:59:39 -0500

    add select statement

---
+++ lib/benziga_article_scraper.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_article(url) -> str:
+    """ """
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    article = soup.find(id="article-body")
+    return article.text

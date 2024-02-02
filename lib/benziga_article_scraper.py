54e650d8a80ef4c0fddd9d90221f68ae50b66f53
Raul Martinez <raulmartinez1855@gmail.com>
2024-02-02 12:32:16 -0500

    add log if encountering throttling

--- lib/benziga_article_scraper.py
+++ lib/benziga_article_scraper.py
@@ -8,6 +8,7 @@ def scrape_article(url) -> str:
     page = requests.get(url)
     status_code = page.status_code
     if status_code == 429:
+        print("TOO MANY REQUESTS")
         time.sleep(30)
     soup = BeautifulSoup(page.content, "html.parser")
     article = soup.find(id="article-body")

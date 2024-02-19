9731bf2e4810d271d2eba5a16ed412cd202e0749
Raul Martinez <raulmartinez1855@gmail.com>
2024-02-19 18:30:16 -0500

    all data preprocessing into one function

---
+++ lib/supervised_learning_data_preprocessing_functions.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import yfinance
+from db_helper_functions import get_stock_news_with_finbert_whole_article_scores_from_db
+from constants import DATA_END_DATE, DATA_START_DATE
+
+
+def gen_df_for_supervised_learning(ticker: str):
+    sentiment_articles_df = get_stock_news_with_finbert_whole_article_scores_from_db(
+        ticker
+    )
+    grouped_sentiments = (
+        sentiment_articles_df.groupby("date", as_index=False)
+        .agg({"positive": "mean", "negative": "mean", "neutral": "mean"})
+        .sort_values(by="date", ascending=True)
+    )
+    stock_price_history = (
+        yfinance.Ticker(ticker)
+        .history(start=DATA_START_DATE, end=DATA_END_DATE)
+        .reset_index()
+    )
+    stock_price_history.columns = [
+        "_".join(x.lower().split(" ")) for x in stock_price_history.columns
+    ]
+    stock_price_history["date"] = stock_price_history["date"].dt.date
+
+    combo_df = pd.merge(
+        stock_price_history,
+        grouped_sentiments,
+        left_on="date",
+        right_on="date",
+        how="left",
+    )
+
+    combo_df["date"] = pd.to_datetime(combo_df["date"])
+    combo_df = combo_df.sort_values(by="date", ascending=True)
+    combo_df = combo_df.set_index("date")
+    combo_df["day_of_month"] = combo_df.index.day
+    combo_df["day_of_week"] = combo_df.index.dayofweek
+    combo_df["quarter"] = combo_df.index.quarter
+    combo_df["month"] = combo_df.index.month
+    combo_df["year"] = combo_df.index.year
+    combo_df["month_year"] = combo_df.index.to_period("M")
+    combo_df["week_year"] = combo_df.index.to_period("W")
+    combo_df[["positive", "negative", "neutral"]] = combo_df[
+        ["positive", "negative", "neutral"]
+    ].ffill()
+    combo_df[["positive", "negative", "neutral"]] = combo_df[
+        ["positive", "negative", "neutral"]
+    ].shift(1)
+    combo_df[["prev_high", "prev_low", "prev_close", "prev_volume"]] = combo_df[
+        ["high", "low", "close", "volume"]
+    ].shift(1)
+    combo_df = combo_df.iloc[1:]
+
+    return combo_df
